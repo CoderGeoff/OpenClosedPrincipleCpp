476f8d88dbfd7f568e6ba4b49a02f4db9ef5c42b
Geoff Bones <geoff.bones@siemens.com>
2015-08-19 13:20:28 +0100

    Correct whitespace

--- DiceGame/Scorer.cpp
+++ DiceGame/Scorer.cpp
@@ -5,15 +5,15 @@ int Scorer::Score(const std::vector<int>& diceThrows) const
 {
     int score = 0;
 
-        for (int diceThrow = 1; diceThrow <= 6; ++diceThrow)
+    for (int diceThrow = 1; diceThrow <= 6; ++diceThrow)
+    {
+        int scoreMultiplier = diceThrow == 1 ? 10 : diceThrow;
+        int numberOfDuplicatesFound = std::count(diceThrows.begin(), diceThrows.end(), diceThrow);
+        if (numberOfDuplicatesFound >= 3)
         {
-            int scoreMultiplier = diceThrow == 1 ? 10 : diceThrow;
-            int numberOfDuplicatesFound = std::count(diceThrows.begin(), diceThrows.end(), diceThrow);
-            if (numberOfDuplicatesFound >= 3)
-            {
-                score += (100 * scoreMultiplier) * static_cast<int>(pow(2, numberOfDuplicatesFound - 3));
-            }
+            score += (100 * scoreMultiplier) * static_cast<int>(pow(2, numberOfDuplicatesFound - 3));
         }
+    }
 
     if (std::count(diceThrows.begin(), diceThrows.end(), 5) == 1)
         score += 50;

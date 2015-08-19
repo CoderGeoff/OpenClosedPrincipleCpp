78321e5f332252592418033832cfecedc79471ee
Geoff Bones <geoff.bones@siemens.com>
2015-08-19 12:57:05 +0100

    One "1" scores 100

--- DiceGame/Scorer.cpp
+++ DiceGame/Scorer.cpp
@@ -6,6 +6,9 @@ int Scorer::Score(const std::vector<int>& diceThrows) const
 
     if (std::count(diceThrows.begin(), diceThrows.end(), 5) == 1)
         score += 50;
-    
+
+    if (std::count(diceThrows.begin(), diceThrows.end(), 1) == 1)
+        score += 100;
+
     return score;
 }
--- DiceGameTests/DiceGameTests.cpp
+++ DiceGameTests/DiceGameTests.cpp
@@ -13,3 +13,11 @@ TEST(DiceGameTests, GivenOneDie_WhenFiveIsThrown_ScoreShouldBe50)
     ASSERT_EQ(50, score);
 }
 
+TEST(DiceGameTests, GivenOneDie_WhenOneIsThrown_ScoreShouldBe100)
+{
+    auto dice = std::vector<int> { 1 };
+    Scorer scorer;
+    int score = scorer.Score(dice);
+    ASSERT_EQ(100, score);
+}
+

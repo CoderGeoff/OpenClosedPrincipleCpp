9843441414236a23833e58acb81f3b7335ad3712
Geoff Bones <geoff.bones@siemens.com>
2015-08-19 16:31:59 +0100

    Parameterize the test in anticipation of a new implementation

--- DiceGameTests/DiceGameTests.cpp
+++ DiceGameTests/DiceGameTests.cpp
@@ -4,15 +4,24 @@
 #include <gtest/gtest.h>
 #include "Scorer.h"
 
-TEST(DiceGameTests, GivenOneDie_WhenFiveIsThrown_ScoreShouldBe50)
+template <class T>
+class DiceGameTests : public testing::Test
+{
+};
+
+using testing::Types;
+typedef Types<Scorer> ScorerTypes;
+TYPED_TEST_CASE(DiceGameTests, ScorerTypes);
+
+TYPED_TEST(DiceGameTests, GivenOneDie_WhenFiveIsThrown_ScoreShouldBe50)
 {
     auto dice = std::vector<int> { 5 };
-    Scorer scorer(dice);
+    TypeParam scorer(dice);
     int score = scorer.Score();
     ASSERT_EQ(50, score);
 }
 
-TEST(DiceGameTests, GivenOneDie_WhenOneIsThrown_ScoreShouldBe100)
+TYPED_TEST(DiceGameTests, GivenOneDie_WhenOneIsThrown_ScoreShouldBe100)
 {
     auto dice = std::vector<int> { 1 };
     Scorer scorer(dice);
@@ -20,7 +29,7 @@ TEST(DiceGameTests, GivenOneDie_WhenOneIsThrown_ScoreShouldBe100)
     ASSERT_EQ(100, score);
 }
 
-TEST(DiceGameTests, GivenThreeDie_WhenAllOnesAreThrown_ScoreShouldBe1000)
+TYPED_TEST(DiceGameTests, GivenThreeDie_WhenAllOnesAreThrown_ScoreShouldBe1000)
 {
     auto dice = std::vector<int> { 1, 1, 1 };
     Scorer scorer(dice);
@@ -28,7 +37,7 @@ TEST(DiceGameTests, GivenThreeDie_WhenAllOnesAreThrown_ScoreShouldBe1000)
     ASSERT_EQ(1000, score);
 }
 
-TEST(DiceGameTests, GivenThreeDie_WhenAllTwosAreThrown_ScoreShouldBe200)
+TYPED_TEST(DiceGameTests, GivenThreeDie_WhenAllTwosAreThrown_ScoreShouldBe200)
 {
     auto dice = std::vector<int> { 2, 2, 2 };
     Scorer scorer(dice);
@@ -36,7 +45,7 @@ TEST(DiceGameTests, GivenThreeDie_WhenAllTwosAreThrown_ScoreShouldBe200)
     ASSERT_EQ(200, score);
 }
 
-TEST(DiceGameTests, GivenThreeDie_WhenAllSixesAreThrown_ScoreShouldBe600)
+TYPED_TEST(DiceGameTests, GivenThreeDie_WhenAllSixesAreThrown_ScoreShouldBe600)
 {
     auto dice = std::vector<int> { 6, 6, 6 };
     Scorer scorer(dice);
@@ -44,7 +53,7 @@ TEST(DiceGameTests, GivenThreeDie_WhenAllSixesAreThrown_ScoreShouldBe600)
     ASSERT_EQ(600, score);
 }
 
-TEST(DiceGameTests, GivenFourDie_WhenAllSixesAreThrown_ScoreShouldBe1200)
+TYPED_TEST(DiceGameTests, GivenFourDie_WhenAllSixesAreThrown_ScoreShouldBe1200)
 {
     auto dice = std::vector<int> { 6, 6, 6, 6 };
     Scorer scorer(dice);
@@ -52,7 +61,7 @@ TEST(DiceGameTests, GivenFourDie_WhenAllSixesAreThrown_ScoreShouldBe1200)
     ASSERT_EQ(1200, score);
 }
 
-TEST(DiceGameTests, GivenFiveDie_WhenAllOnesAreThrown_ScoreShouldBe4000)
+TYPED_TEST(DiceGameTests, GivenFiveDie_WhenAllOnesAreThrown_ScoreShouldBe4000)
 {
     auto dice = std::vector<int> { 1, 1, 1, 1, 1 };
     Scorer scorer(dice);
@@ -60,7 +69,7 @@ TEST(DiceGameTests, GivenFiveDie_WhenAllOnesAreThrown_ScoreShouldBe4000)
     ASSERT_EQ(4000, score);
 }
 
-TEST(DiceGameTests, GivenSixDie_WhenAllThreesAreThrown_ScoreShouldBe2400)
+TYPED_TEST(DiceGameTests, GivenSixDie_WhenAllThreesAreThrown_ScoreShouldBe2400)
 {
     auto dice = std::vector<int> { 3, 3, 3, 3, 3, 3 };
     Scorer scorer(dice);
@@ -68,7 +77,7 @@ TEST(DiceGameTests, GivenSixDie_WhenAllThreesAreThrown_ScoreShouldBe2400)
     ASSERT_EQ(2400, score);
 }
 
-TEST(DiceGameTests, GivenTwoDie_WhenTwoFivesAreThrownn_ScoreShouldBe100)
+TYPED_TEST(DiceGameTests, GivenTwoDie_WhenTwoFivesAreThrownn_ScoreShouldBe100)
 {
     auto dice = std::vector<int> { 5, 5 };
     Scorer scorer(dice);
@@ -76,14 +85,15 @@ TEST(DiceGameTests, GivenTwoDie_WhenTwoFivesAreThrownn_ScoreShouldBe100)
     ASSERT_EQ(100, score);
 }
 
-TEST(DiceGameTests, GivenTwoDie_WhenTwoOnesAreThrown_ScoreShouldBe200)
+TYPED_TEST(DiceGameTests, GivenTwoDie_WhenTwoOnesAreThrown_ScoreShouldBe200)
 {
     auto dice = std::vector<int> { 1, 1 };
     Scorer scorer(dice);
     int score = scorer.Score();
     ASSERT_EQ(200, score);
 }
-TEST(DiceGameTests, GivenSixDie_FullStraight_ScoreShouldBe1500)
+
+TYPED_TEST(DiceGameTests, GivenSixDie_FullStraight_ScoreShouldBe1500)
 {
     auto dice = std::vector<int> { 1, 2, 3, 4, 5, 6 };
     Scorer scorer(dice);

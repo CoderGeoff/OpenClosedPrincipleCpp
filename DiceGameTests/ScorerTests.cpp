9f99e86c3c157858c2fb19c992eadd5d3d39b96d
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-08-20 12:03:27 +0100

    Update tests

--- DiceGameTests/ScorerTests.cpp
+++ DiceGameTests/ScorerTests.cpp
@@ -39,7 +39,7 @@ TYPED_TEST(ScorerTests, GivenOneDie_WhenFiveIsThrown_ScoreShouldBe50)
 TYPED_TEST(ScorerTests, GivenOneDie_WhenOneIsThrown_ScoreShouldBe100)
 {
     auto dice = std::vector<int> { 1 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(100, score);
 }
@@ -47,7 +47,7 @@ TYPED_TEST(ScorerTests, GivenOneDie_WhenOneIsThrown_ScoreShouldBe100)
 TYPED_TEST(ScorerTests, GivenThreeDie_WhenAllOnesAreThrown_ScoreShouldBe1000)
 {
     auto dice = std::vector<int> { 1, 1, 1 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(1000, score);
 }
@@ -55,7 +55,7 @@ TYPED_TEST(ScorerTests, GivenThreeDie_WhenAllOnesAreThrown_ScoreShouldBe1000)
 TYPED_TEST(ScorerTests, GivenThreeDie_WhenAllTwosAreThrown_ScoreShouldBe200)
 {
     auto dice = std::vector<int> { 2, 2, 2 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(200, score);
 }
@@ -63,7 +63,7 @@ TYPED_TEST(ScorerTests, GivenThreeDie_WhenAllTwosAreThrown_ScoreShouldBe200)
 TYPED_TEST(ScorerTests, GivenThreeDie_WhenAllSixesAreThrown_ScoreShouldBe600)
 {
     auto dice = std::vector<int> { 6, 6, 6 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(600, score);
 }
@@ -71,7 +71,7 @@ TYPED_TEST(ScorerTests, GivenThreeDie_WhenAllSixesAreThrown_ScoreShouldBe600)
 TYPED_TEST(ScorerTests, GivenFourDie_WhenAllSixesAreThrown_ScoreShouldBe1200)
 {
     auto dice = std::vector<int> { 6, 6, 6, 6 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(1200, score);
 }
@@ -79,7 +79,7 @@ TYPED_TEST(ScorerTests, GivenFourDie_WhenAllSixesAreThrown_ScoreShouldBe1200)
 TYPED_TEST(ScorerTests, GivenFiveDie_WhenAllOnesAreThrown_ScoreShouldBe4000)
 {
     auto dice = std::vector<int> { 1, 1, 1, 1, 1 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(4000, score);
 }
@@ -87,7 +87,7 @@ TYPED_TEST(ScorerTests, GivenFiveDie_WhenAllOnesAreThrown_ScoreShouldBe4000)
 TYPED_TEST(ScorerTests, GivenSixDie_WhenAllThreesAreThrown_ScoreShouldBe2400)
 {
     auto dice = std::vector<int> { 3, 3, 3, 3, 3, 3 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(2400, score);
 }
@@ -95,7 +95,7 @@ TYPED_TEST(ScorerTests, GivenSixDie_WhenAllThreesAreThrown_ScoreShouldBe2400)
 TYPED_TEST(ScorerTests, GivenTwoDie_WhenTwoFivesAreThrownn_ScoreShouldBe100)
 {
     auto dice = std::vector<int> { 5, 5 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(100, score);
 }
@@ -103,7 +103,7 @@ TYPED_TEST(ScorerTests, GivenTwoDie_WhenTwoFivesAreThrownn_ScoreShouldBe100)
 TYPED_TEST(ScorerTests, GivenTwoDie_WhenTwoOnesAreThrown_ScoreShouldBe200)
 {
     auto dice = std::vector<int> { 1, 1 };
-    Scorer scorer(dice);
+    TypeParam scorer(this->CreateScorer(dice));
     int score = scorer.Score();
     ASSERT_EQ(200, score);
 }

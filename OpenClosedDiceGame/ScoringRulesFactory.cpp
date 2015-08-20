79e401cccd001dd2730d6644268ba1e9b486b9e0
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-08-20 14:41:28 +0100

    Pretty code

--- OpenClosedDiceGame/ScoringRulesFactory.cpp
+++ OpenClosedDiceGame/ScoringRulesFactory.cpp
@@ -14,21 +14,21 @@ std::vector<std::shared_ptr<IScoringRule>> ScoringRulesFactory::CreateRules() co
     auto five2sTo6sScore400TimesDiceValue = MakeShared(new NOfAKindScoresXRule(twoToSixPredicate, 5, [](int n) { return 400 * n; }));
     auto four2sTo6sScore200TimesDiceValue = MakeShared(new NOfAKindScoresXRule(twoToSixPredicate, 4, [](int n) { return 200 * n; }));
     auto three2sTo6sScore100TimesDiceValue = MakeShared(new NOfAKindScoresXRule(twoToSixPredicate, 3, [](int n) { return 100 * n; }));
+    auto straightFlushScores1500 = MakeShared(new PatternScoresXRule({ 1, 2, 3, 4, 5, 6 }, 1500));
     auto single1Scores100 = MakeShared(new NOfAKindScoresXRule(1, 1, 100));
     auto single5Scores50 = MakeShared(new NOfAKindScoresXRule(5, 1, 50));
-    auto straightFlushScores1500 = MakeShared(new PatternScoresXRule({ 1, 2, 3, 4, 5, 6 }, 1500));
 
     return std::vector<std::shared_ptr<IScoringRule>> 
     {
         six1sScore8000,
         five1sScore4000,
         four1sScore2000,
-        straightFlushScores1500,
         three1sScore1000,
         six2sTo6sScore800TimesDiceValue,
         five2sTo6sScore400TimesDiceValue,
         four2sTo6sScore200TimesDiceValue,
         three2sTo6sScore100TimesDiceValue,
+        straightFlushScores1500,
         single1Scores100,
         single5Scores50
     };

cf5a89794f2ad2fa64e5e40416008494144e71ed
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-08-20 14:40:11 +0100

    Simplify making shared pointers
    compiler has a bug that means it doesn't recognise the initialiser lists

--- OpenClosedDiceGame/ScoringRulesFactory.cpp
+++ OpenClosedDiceGame/ScoringRulesFactory.cpp
@@ -6,17 +6,17 @@ std::vector<std::shared_ptr<IScoringRule>> ScoringRulesFactory::CreateRules() co
 {
     auto twoToSixPredicate = [](int n){ return n != 1; };
 
-    auto six1sScore8000 = std::shared_ptr<PatternScoresXRule>(new PatternScoresXRule({ 1, 1, 1, 1, 1, 1 }, 8000));
-    auto five1sScore4000 = std::shared_ptr<PatternScoresXRule>(new PatternScoresXRule({ 1, 1, 1, 1, 1 }, 4000));
-    auto four1sScore2000 = std::shared_ptr<PatternScoresXRule>(new PatternScoresXRule({ 1, 1, 1, 1 }, 2000));
-    auto three1sScore1000 = std::shared_ptr<PatternScoresXRule>(new PatternScoresXRule({ 1, 1, 1 }, 1000));
-    auto six2sTo6sScore800TimesDiceValue = std::make_shared <NOfAKindScoresXRule>(twoToSixPredicate, 6, [](int n) { return 800 * n; });
-    auto five2sTo6sScore400TimesDiceValue = std::make_shared <NOfAKindScoresXRule>(twoToSixPredicate, 5, [](int n) { return 400 * n; });
-    auto four2sTo6sScore200TimesDiceValue = std::make_shared <NOfAKindScoresXRule>(twoToSixPredicate, 4, [](int n) { return 200 * n; });
-    auto three2sTo6sScore100TimesDiceValue = std::make_shared <NOfAKindScoresXRule>(twoToSixPredicate, 3, [](int n) { return 100 * n; });
-    auto single1Scores100 = std::make_shared <NOfAKindScoresXRule>(1, 1, 100);
-    auto single5Scores50 = std::make_shared <NOfAKindScoresXRule>(5, 1, 50);
-    auto straightFlushScores1500 = std::shared_ptr<PatternScoresXRule>(new PatternScoresXRule({ 1, 2, 3, 4, 5, 6 }, 1500));
+    auto six1sScore8000 = MakeShared(new PatternScoresXRule({ 1, 1, 1, 1, 1, 1 }, 8000));
+    auto five1sScore4000 = MakeShared(new PatternScoresXRule({ 1, 1, 1, 1, 1 }, 4000));
+    auto four1sScore2000 = MakeShared(new PatternScoresXRule({ 1, 1, 1, 1 }, 2000));
+    auto three1sScore1000 = MakeShared(new PatternScoresXRule({ 1, 1, 1 }, 1000));
+    auto six2sTo6sScore800TimesDiceValue = MakeShared(new NOfAKindScoresXRule(twoToSixPredicate, 6, [](int n) { return 800 * n; }));
+    auto five2sTo6sScore400TimesDiceValue = MakeShared(new NOfAKindScoresXRule(twoToSixPredicate, 5, [](int n) { return 400 * n; }));
+    auto four2sTo6sScore200TimesDiceValue = MakeShared(new NOfAKindScoresXRule(twoToSixPredicate, 4, [](int n) { return 200 * n; }));
+    auto three2sTo6sScore100TimesDiceValue = MakeShared(new NOfAKindScoresXRule(twoToSixPredicate, 3, [](int n) { return 100 * n; }));
+    auto single1Scores100 = MakeShared(new NOfAKindScoresXRule(1, 1, 100));
+    auto single5Scores50 = MakeShared(new NOfAKindScoresXRule(5, 1, 50));
+    auto straightFlushScores1500 = MakeShared(new PatternScoresXRule({ 1, 2, 3, 4, 5, 6 }, 1500));
 
     return std::vector<std::shared_ptr<IScoringRule>> 
     {
@@ -32,4 +32,9 @@ std::vector<std::shared_ptr<IScoringRule>> ScoringRulesFactory::CreateRules() co
         single1Scores100,
         single5Scores50
     };
+}
+
+std::shared_ptr<IScoringRule> ScoringRulesFactory::MakeShared(IScoringRule* rule) const
+{
+    return std::shared_ptr<IScoringRule>(rule);
 }
--- OpenClosedDiceGame/ScoringRulesFactory.h
+++ OpenClosedDiceGame/ScoringRulesFactory.h
@@ -5,6 +5,7 @@ class IScoringRule;
 
 class ScoringRulesFactory
 {
+    std::shared_ptr<IScoringRule> MakeShared(IScoringRule * rule) const;
 public:
     std::vector<std::shared_ptr<IScoringRule>> CreateRules() const;
 };

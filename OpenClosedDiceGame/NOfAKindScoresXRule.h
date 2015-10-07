28ae627c75f584935fa99211a63dbcbd223d1e4d
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-10-07 22:54:51 +0530

    Remove unused method declaration

--- OpenClosedDiceGame/NOfAKindScoresXRule.h
+++ OpenClosedDiceGame/NOfAKindScoresXRule.h
@@ -8,8 +8,6 @@ class NOfAKindScoresXRule : public IScoringRule
     int m_Count;
     std::function<int(int)> m_ScoreEvaluator;
 
-    bool TryRemoveMatchingDiceFrom(std::map<int, int>& diceCount) const;
-
 public:
     NOfAKindScoresXRule(std::function<bool(int)> valuePredicate, int count, std::function<int(int)> scoreEvaluator);
     ScoringRuleResult TryMatch(const std::map<int, int>& diceCount) const override;
